089ffb2739456fd105616c339effd0508ffc389c
Cijo <sunnycijo@gmail.com>
2020-11-06 13:13:20 +0530

    Kubernetes Deployment And Service Files

--- sales-api/src/main/java/org/erp/sales/client/customer/CustomerClient.java
+++ sales-api/src/main/java/org/erp/sales/client/customer/CustomerClient.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 //@FeignClient(name = "customer-api")
 @FeignClient(name = "edge-server-api")
 public interface CustomerClient {
+//	@GetMapping("/info/v1/idfromphone")
 	@GetMapping("/customer-api/info/v1/idfromphone")
 	public String getCustomerIdFromPhone();
 }
